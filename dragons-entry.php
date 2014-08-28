66d3879907bd15aa47c3a61f4ffc1bdd55266af1
tomcatt360 <tomcatt360@gmail.com>
2014-08-28 10:45:45 -0500

    Fix some broken closing tags

--- dragons-entry.php
+++ dragons-entry.php
@@ -1,9 +1,9 @@
 <html>
  <head>
   <title>Dragons Mad Lib By SAM</title>
- <Head>
+  </head>
  <body>
   <p><h1>Dragons Mad Lib</h1></p>
-  <p><h5>Contributed by SAM at <a href="http://www.madglibs.com">Madglibs.com</a><h5></p>
+   <p><h5>Contributed by SAM at <a href="http://www.madglibs.com">Madglibs.com</a></h5></p>
  </body>
 </html>
