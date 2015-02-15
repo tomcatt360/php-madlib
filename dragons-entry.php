093792b4daf8c82f2d4767adcf95538feeb5c7cc
Thomas Parker <tomcatt360@gmail.com>
2015-02-14 17:35:03 -0700

    add tab indention to dragons-entry.php

--- dragons-entry.php
+++ dragons-entry.php
@@ -1,9 +1,13 @@
 <html>
- <head>
-  <title>Dragons Mad Lib By SAM</title>
-  </head>
- <body>
-  <p><h1>Dragons Mad Lib</h1></p>
-   <p><h5>Contributed by SAM at <a href="http://www.madglibs.com">Madglibs.com</a></h5></p>
- </body>
+	<head>
+		<title>Dragons Mad Lib By SAM</title>
+	</head>
+	<body>
+  		<p>
+			<h1>Dragons Mad Lib</h1>
+		</p>
+		<p>
+			<h5>Contributed by SAM at <a href="http://www.madglibs.com">Madglibs.com</a></h5>
+		</p>
+	</body>
 </html>
